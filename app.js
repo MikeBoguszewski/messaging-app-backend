f90beebd41c6bc05ff6701b89bc1c53c92f8e735
MikeBoguszewski <133729602+MikeBoguszewski@users.noreply.github.com>
2024-03-17 15:46:29 -0500

    Implement user authentication with passport

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const authRouter = require("./routes/auth");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGO_URL;
@@ -8,3 +24,18 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+
+app.use("/", authRouter);
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+const router = express.Router();
+
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, async function (email, password, done) {
+    try {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return done(null, false, { message: "Incorrect Email" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect Password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    if (!user) {
+      return res.status(401).json({ error: "Incorrect email or password" });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+      return res.status(200).json({ message: "Login successful", user });
+    });
+  })(req, res, next);
+});
+
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+  });
+  res.status(200).json({ message: "Logout successful" });
+});
+
+router.post("/signup", async function (req, res, next) {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(409).json({ message: "Account already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ email: email, username: username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
